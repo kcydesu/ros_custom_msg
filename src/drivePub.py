059f8a6761588d01f3047be5bb4a0d0f3efb5575
kcydesu <38144480+kcydesu@users.noreply.github.com>
2019-12-03 16:05:13 -0500

    Created the example publisher

---
+++ src/drivePub.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from templecar.msg import Drive
+import random
+
+def talker():
+    pub = rospy.Publisher('drive_controls', Drive, queue_size=10)
+    rospy.init_node('drive_commands', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        # Picks two random numbers in the range for our car
+        steering = random.randint(12,23)
+        speed = random.randint(12,23)
+        
+        # Creates a 'blank' Drive message and sets values
+        msg = Drive()
+        msg.steering = steering
+        msg.speed = speed
+        
+        # Logs and publishes message
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
